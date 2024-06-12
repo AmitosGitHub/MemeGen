b5354b61b39619af5146a62308af4f39c5ce9f32
AmitOs <106267735+AmitosGitHub@users.noreply.github.com>
2024-06-12 04:08:37 +0300

    commit07 -resize+mq
    fix media query + resize canvas

--- js/controller/main-controller.js
+++ js/controller/main-controller.js
@@ -6,15 +6,25 @@ var gCtx
 function onInit() {
   renderGallery()
   InitCanvas()
+  window.addEventListener('resize', () => {
+    resizeCanvas()
+    renderCanvas()
+  })
 }
 
 function InitCanvas() {
   gElCanvas = document.querySelector('.main-canvas')
   gCtx = gElCanvas.getContext('2d')
-
-  renderCanvas()
 }
 
 function onToggleMenu() {
   document.body.classList.toggle('open-menu')
+}
+
+function resizeCanvas() {
+  var elContainer = document.querySelector('.canvas-container')
+  // Note: changing the canvas dimension this way clears the canvas
+  gElCanvas.width = elContainer.offsetWidth
+  // Unless needed, better keep height fixed.
+  gElCanvas.height = elContainer.offsetHeight
 }
